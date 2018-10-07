731cf387df7d2b57da76b51fe5eec0e7af3bf007
adeluleye <adelu4real@gmail.com>
2018-10-07 01:19:40 +0100

    setup views in view controller, add background image view to view and set anchors on image view

--- CollectionViewNSB/ViewController.swift
+++ CollectionViewNSB/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let backgroundImageView: UIImageView = {
+        let image = UIImage(named: "bg")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.addSubview(backgroundImageView)
+        
+        backgroundImageView.setAnchor(top: view.topAnchor, topPad: 0, bottom: view.bottomAnchor, bottomPad: 0, left: view.leftAnchor, leftPad: 0, right: view.rightAnchor, rightPad: 0, height: 0, width: 0)
     }
 
 
